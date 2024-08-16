990708de981cd0408194261d12843feda20ff6c0
Stefano Vidaurre Olite <fano494@gmail.com>
2024-08-16 20:13:26 +0200

    feat: repository with logic delete

--- src/CCSV.Domain/Repositories/IRepository.cs
+++ src/CCSV.Domain/Repositories/IRepository.cs
@@ -4,13 +4,16 @@ namespace CCSV.Domain.Repositories;
 
 public interface IRepository<TEntity> where TEntity : Entity
 {
-    Task<int> Count(Func<IQueryable<TEntity>, IQueryable<TEntity>>? query = null);
-    Task<bool> Any(Func<IQueryable<TEntity>, IQueryable<TEntity>>? query = null);
+    Task<int> Count(bool deletionsIncluded = false);
+    Task<int> Count(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool deletionsIncluded = false);
+    Task<bool> Any(bool deletionsIncluded = false);
+    Task<bool> Any(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool deletionsIncluded = false);
     Task<TEntity?> FindOrDefault(Guid id);
     Task<TEntity> Find(Guid id);
     Task<TEntity?> GetByIdOrDefault(Guid id);
     Task<TEntity> GetById(Guid id);
-    Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IQueryable<TEntity>>? query = null);
+    Task<IEnumerable<TEntity>> GetAll(bool deletionsIncluded = false);
+    Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IQueryable<TEntity>>query, bool deletionsIncluded = false);
     Task Create(TEntity entity);
     Task Update(TEntity entity);
     Task Delete(TEntity entity);
