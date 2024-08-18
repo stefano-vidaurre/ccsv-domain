86555861b9a5443c588c6ef040fb80c283827153
Stefano Vidaurre Olite <fano494@gmail.com>
2024-08-18 21:06:18 +0200

    chore: stable version

--- src/CCSV.Domain/Repositories/IRepository.cs
+++ src/CCSV.Domain/Repositories/IRepository.cs
@@ -4,16 +4,16 @@ namespace CCSV.Domain.Repositories;
 
 public interface IRepository<TEntity> where TEntity : Entity
 {
-    Task<int> Count(bool deletionsIncluded = false);
-    Task<int> Count(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool deletionsIncluded = false);
-    Task<bool> Any(bool deletionsIncluded = false);
-    Task<bool> Any(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool deletionsIncluded = false);
+    Task<int> Count(bool disabledIncluded = false);
+    Task<int> Count(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool disabledIncluded = false);
+    Task<bool> Any(bool disabledIncluded = false);
+    Task<bool> Any(Func<IQueryable<TEntity>, IQueryable<TEntity>> query, bool disabledIncluded = false);
     Task<TEntity?> FindOrDefault(Guid id);
     Task<TEntity> Find(Guid id);
     Task<TEntity?> GetByIdOrDefault(Guid id);
     Task<TEntity> GetById(Guid id);
-    Task<IEnumerable<TEntity>> GetAll(bool deletionsIncluded = false);
-    Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IQueryable<TEntity>>query, bool deletionsIncluded = false);
+    Task<IEnumerable<TEntity>> GetAll(bool disabledIncluded = false);
+    Task<IEnumerable<TEntity>> GetAll(Func<IQueryable<TEntity>, IQueryable<TEntity>>query, bool disabledIncluded = false);
     Task Create(TEntity entity);
     Task Update(TEntity entity);
     Task Delete(TEntity entity);
