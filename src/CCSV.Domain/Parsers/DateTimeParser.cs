1d13b7f727e4fbe79e9f5d3aa498d75b6bfa0e93
Stefano Vidaurre Olite <fano494@gmail.com>
2025-05-20 13:27:31 +0200

    feat: guid parser

--- src/CCSV.Domain/Parsers/DateTimeParser.cs
+++ src/CCSV.Domain/Parsers/DateTimeParser.cs
@@ -7,20 +7,50 @@ public static class DateTimeParser
 {
     public static DateTime ParseUTC(string? value)
     {
-        if (string.IsNullOrWhiteSpace(value))
+        return Parse(value).ToUniversalTime();
+    }
+    public static DateTime ParseUTC(Guid value)
+    {
+        return Parse(value).ToUniversalTime();
+    }
+
+    public static bool TryParseUTC(string? value, out DateTime parsed)
+    {
+        if (!TryParse(value, out parsed))
         {
-            throw new InvalidValueException($"DateTime UTC value cant be null/empty.");
+            return false;
         }
 
-        if (!TryParseUTC(value, out DateTime result))
+        parsed = parsed.ToUniversalTime();
+        return true;
+    }
+
+    public static DateTime Parse(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
         {
-            throw new InvalidValueException($"DateTime UTC value ({value}) is not valid.");
+            throw new InvalidValueException($"DateTime value cant be null/empty.");
+        }
+
+        if (!TryParse(value, out DateTime result))
+        {
+            throw new InvalidValueException($"DateTime value ({value}) is not valid.");
         }
 
         return result;
     }
 
-    public static bool TryParseUTC(string? value, out DateTime parsed)
+    public static DateTime Parse(Guid value)
+    {
+        byte[] bytes = value.ToByteArray();
+
+        long ticks = BitConverter.ToInt64(bytes, 0);
+        DateTimeKind kind = (DateTimeKind)bytes[8];
+
+        return new DateTime(ticks, kind);
+    }
+
+    public static bool TryParse(string? value, out DateTime parsed)
     {
         if (value is null)
         {
@@ -34,7 +64,54 @@ public static class DateTimeParser
             return false;
         }
 
+        return true;
+    }
+
+    public static DateTime ParseUTCExact(string? value, string format)
+    {
+        return ParseExact(value, format).ToUniversalTime();
+    }
+
+    public static bool TryParseUTCExact(string? value, string format, out DateTime parsed)
+    {
+        if (!TryParseExact(value, format, out parsed))
+        {
+            return false;
+        }
+
         parsed = parsed.ToUniversalTime();
+        return true;
+    }
+
+    public static DateTime ParseExact(string? value, string format)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new InvalidValueException($"DateTime value cant be null/empty.");
+        }
+
+        if (!TryParseExact(value, format, out DateTime result))
+        {
+            throw new InvalidValueException($"DateTime value ({value}) is not valid.");
+        }
+
+        return result;
+    }
+
+    public static bool TryParseExact(string? value, string format, out DateTime parsed)
+    {
+        if (value is null)
+        {
+            parsed = default;
+            return false;
+        }
+
+        if (!DateTime.TryParseExact(value, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsed))
+        {
+            parsed = default;
+            return false;
+        }
+
         return true;
     }
 }
---
+++ src/CCSV.Domain/Parsers/GuidParser.cs
@@ -0,0 +1,20 @@
+﻿namespace CCSV.Domain.Parsers;
+
+public static class GuidParser
+{
+    const int GuidLength = 16;
+    const int DateTimeLength = 8;
+
+    public static Guid Parse(DateTime value)
+    {
+        byte[] guidBytes = new byte[GuidLength];
+
+        byte[] dateBytes = BitConverter.GetBytes(value.Ticks);
+        byte kindByte = (byte)value.Kind;
+
+        Array.Copy(dateBytes, 0, guidBytes, 0, DateTimeLength);
+        guidBytes[DateTimeLength] = kindByte;
+
+        return new Guid(guidBytes);
+    }
+}
